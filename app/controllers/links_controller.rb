c22d517be85b0440d14132022f72380e4923235f
Vaibhav Gadewar <vaibhav.gadewar@gmail.com>
2015-06-10 12:26:16 +0530

    links on homepage

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,62 @@
+class LinksController < ApplicationController
+	
+	def show
+		@link = Link.all
+
+    	respond_to do |format|
+      		format.html # show.html.erb
+      		format.json { render json: @link }
+    	end
+	end
+
+	def new
+		@link = Link.new
+		
+		respond_to do |format|
+      		format.html # new.html.erb
+      		format.json { render json: @link }
+    	end
+	end
+
+	def create
+		@link = Link.new(params[:link])
+
+    	respond_to do |format|
+    	  if @link.save
+        	format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        	format.json { render json: @link, status: :created, location: @link }
+      	  else
+        	format.html { render action: "new" }
+        	format.json { render json: @link.errors, status: :unprocessable_entity }
+      	  end
+    	end
+	end
+
+	def edit
+    	@link = Link.find(params[:id])
+	end
+
+	def update
+      @link = Link.find(params[:id])
+
+      respond_to do |format|
+      	if @link.update_attributes(params[:idea])
+      		format.html { redirect_to @link, notice: 'Link was successfully updated.' }
+        	format.json { head :no_content }
+      	else
+        	format.html { render action: "edit" }
+       		format.json { render json: @link.errors, status: :unprocessable_entity }
+      	end
+   	  end
+  	end
+
+  	def destroy
+    	@link = Link.find(params[:id])
+    	@link.destroy
+
+    	respond_to do |format|
+      		format.html { redirect_to links_url }
+      		format.json { head :no_content }
+    	end
+  	end
+end
